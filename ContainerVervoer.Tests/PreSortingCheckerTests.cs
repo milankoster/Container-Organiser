58f554631c71c872a7ef1ddb826e7a74019c0c0b
Milan Koster <m.kostervangroos@student.fontys.nl>
2020-11-27 15:42:15 +0100

    Added Config and unit tests

---
+++ ContainerVervoer.Tests/PreSortingCheckerTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace ContainerVervoer.Tests
+{
+    public class PreSortingCheckerTests
+    {
+        private Config _config = new Config();
+        private Ship _ship;
+
+        public void InvalidMaxWeight()
+        {
+            
+        }
+    }
+}
